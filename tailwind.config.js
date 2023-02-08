70b773c1ef3b196c2de273787e6a20d680c12bcf
fateme <fateme.3.abed@gmail.com>
2023-02-08 19:49:11 +0330

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'iran-sans': ['iran-sans'],
+        // 'iran': ['iran-sans'],
+      },
+    },
   },
   plugins: [],
 }
